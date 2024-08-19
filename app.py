9dc08f2c42215ddcef0055969ce7b3c4a08e0701
Ogunaru <72740815+5ignal@users.noreply.github.com>
2024-08-19 05:04:17 +0000

    discord webhook 알림 추가

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 class cloudflare_ddns():
-    def __init__(self, domain: str, token: str, proxy: bool):
+    def __init__(self, domain: str, token: str, proxy: bool, webhook: str):
         self.cloudflare_api = "https://api.cloudflare.com/client/v4/zones"
         self.headers = {
             "Authorization": f"Bearer {token}",
@@ -12,6 +12,7 @@ class cloudflare_ddns():
         ipAddr = self.getIP()
         splitDomain = domain.split(".")
         if len(splitDomain) == 2:
+            splitDomain = domain
             idZone = self.getIdZone(domain)
         else:
             splitDomain = f"{splitDomain[1]}.{splitDomain[2]}"
@@ -22,6 +23,7 @@ class cloudflare_ddns():
             return
         else:
             update = self.update(domain, idZone, record[0], ipAddr, proxy)
+            self.discord(webhook, update, domain, ipAddr, self.getIdAccount(), splitDomain)
 
     def getIP(self) -> str:
         res = requests.get("https://myip.ogunaru.workers.dev").text
@@ -63,12 +65,46 @@ class cloudflare_ddns():
         data = json.loads(response.text)
         if data.get("success") == True:
             print("DNS Update Success")
+            return True
         else:
             print("DNS Update Failed")
             print(response.text)
+            return False
+
+    def getIdAccount(self):
+        response = requests.get(
+            "https://api.cloudflare.com/client/v4/accounts",
+            headers=self.headers)
+        data = json.loads(response.text)["result"]
+        if data[0]:
+            return data[0]['id']
+        else:
+            print("Account Not Found")
+
+    def discord(self, webhook, update, domain, ipAddr, idAccount, idZone):
+        color = int("e7883b", 16) if update else int("e01e5a", 16)
+        text = "Success" if update else "Failed"
+
+        author_url = f"https://dash.cloudflare.com/{idAccount}/{idZone}/dns/records"
+        icon_url = "https://yt3.googleusercontent.com/lyrJ4UAHQLNGpkLQbL03Xh6GJvAZxA1loSDBRYPWuRbssAoEdCsN0DeybqdKNiJH7KA9NsoH-w=s900-c-k-c0x00ffffff-no-rj"
+
+        payload = dict()
+        payload["content"] = "@here"
+        payload["embeds"] = list()
+        payload_embed = dict()
+        payload_embed["color"] = color
+        payload_embed["author"] = dict([("name", "Cloudflare"), ("url", author_url), ("icon_url", icon_url)])
+        payload_embed["fields"] = [dict([("name", "A Record"), ("value", domain), ("inline", True)]),
+                               dict([("name", "IP Address"), ("value", ipAddr), ("inline", True)])]
+        payload_embed["title"] = f"DDNS Update {text}"
+        payload["embeds"].append(payload_embed)
+
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(webhook, data=json.dumps(payload), headers=headers)
+        print(response.status_code, response.text)
 
 if __name__ == "__main__":
     with open("./config.json", "r") as f:
         inp = json.load(f)
     for i in inp:
-        cloudflare_ddns(i["cloudflare_domain"], i["cloudflare_api_token"], i["cloudflare_proxy"])
+        cloudflare_ddns(i["cloudflare_domain"], i["cloudflare_api_token"], i["cloudflare_proxy"], i["webhook"])
